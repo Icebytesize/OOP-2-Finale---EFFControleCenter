0aa9a2673a427ab917d37ae9b50861294823f393
Icebytesize <Matthiasuk@gmail.com>
2026-09-10 21:15:29 +0200

    Changen cs.proj to be able to use winforms

--- OOP 2 Finale - EFFControleCenter/Program.cs
+++ OOP 2 Finale - EFFControleCenter/Program.cs
@@ -5,29 +5,4 @@ using OOP_2_Finale___EFFControleCenter.Units;
 
 
 
-List<Pilot> EmptyPilots = new List<Pilot>();
-await JsonService.OverwriteJson("pilots.json", EmptyPilots);
-
-
-var controlCenter = new EFFControlCenter();
-
-
-
-int nextId = await JsonService.GetNextId<Pilot>("pilots.json");
-
-/*
-foreach (Pilot pilot in pilots)
-{
-    pilot.Id = nextId;
-    nextId++;
-}
-*/
-
-//await JsonService.OverwriteJson("pilots.json", pilots);
-
-List<Pilot> loadedPilots = await JsonService.LoadFromJson<Pilot>("pilots.json");
-
-foreach (Pilot pilot in loadedPilots)
-{
-    Console.WriteLine($"Pilot: {pilot.Name}, ID: {pilot.Id}");
-}
+var controlCenter = await DataLoader.LoadAllData();
